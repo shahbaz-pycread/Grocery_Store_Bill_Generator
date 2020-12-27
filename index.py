6598fe9f6947259d38bc0ddd46c4d20cb4bf6f0a
MOHAMMAD SHAHBAZ ALAM <alam.shahbaz01@gmail.com>
2020-12-27 11:29:12 +0530

    Update index.py

--- index.py
+++ index.py
@@ -8,20 +8,24 @@ sum = 0
 count=0
 item_list = []
 item_cost = []
+item_quantity = []
 while(True):
     item = input("Enter the item name: ")
     userInput = input("Enter the price or press q to quit: ")
+    quantity = float(input("Enter Quantity: "))
     if(userInput != 'q'):
-        sum = sum + int(userInput)
+        sum = sum + int(userInput)*quantity
         print(f"Total Bill so far {sum}")
         item_list.append(item)
         item_cost.append(userInput)
+        item_quantity.append(quantity)
         count = count + 1
         
     else:
         print('Bill Id: ',random.randrange(1,10000000))
-        print(item_list) 
-        print(item_cost)
+        print('Items: ', item_list) 
+        print('Price: ', item_cost)
+        print('Quantity: ', item_quantity)
         print(f"Total Price: Rs {sum}")
        
         print("Total Item: ", count)
